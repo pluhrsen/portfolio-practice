266489006ef668fc052b76d538c187f750d0d889
paulluhrsen <paulluhrsen@gmail.com>
2020-01-06 13:19:44 -0600

    movieRanker page and link working

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,19 +3,22 @@ import { HashRouter as Router, Link } from "react-router-dom";
 
 const Main = () => {
   return (
-    <div>
-      <nav>Welcome!</nav>
-      <main>
-        <div>This is my portfolio</div>
-        <div> intro to portfolio</div>
-        <ul>
-          <li>this</li>
-          <li>will</li>
-          <li>be a list</li>
-          <li>of projects</li>
-        </ul>
-      </main>
-    </div>
+    <Router>
+      <div>
+        <nav>Welcome!</nav>
+        <main>
+          <div>This is my portfolio</div>
+          <div> intro to portfolio</div>
+          <ul>
+            <li>this</li>
+            <li>will</li>
+            <li>be a list</li>
+            <li>of projects</li>
+          </ul>
+          <Link to="/movie-rank">Go to Movie Ranker</Link>
+        </main>
+      </div>
+    </Router>
   );
 };
 
